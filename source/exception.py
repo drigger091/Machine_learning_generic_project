8a9a541b0945e95472b3774448a35235079a574e
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-07-20 21:58:50 +0530

    exception custom

---
+++ source/exception.py
@@ -0,0 +1,24 @@
+import sys
+
+def error_message_details(error, error_detail:sys):
+
+    _,_, exc_tb =error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error Occured in python Script name[{0}] line Number[{1}] error Message[{2}]".format(
+    file_name ,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+
+class CustomException(Exception):
+
+    def __init__(self, error_message,error_detail:sys):
+        super.__init__(error_message)
+        self._error_message=error_message_details(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self._error_message
+
+
+
+
