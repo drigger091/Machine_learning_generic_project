4cc69e1c149a4984c3b79e945f68ffe99e64f109
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-08-28 21:14:23 +0530

    update

---
+++ source/pipeline/predict_pipeline.py
@@ -0,0 +1,20 @@
+import sys
+import pandas as pd
+from source.exception import CustomException
+from source.utils import load_object
+
+
+
+
+class PredictPipeline:
+
+    def __init__(self):
+        pass
+
+
+class Customdata:
+    def __init__(self ,
+                 gender:str,
+                 
+        pass
+
